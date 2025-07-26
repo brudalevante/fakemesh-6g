9dc03312e7cd16473d572695398721280af60de7
brudalevante <continentalwl@gmail.com>
2025-07-26 19:22:28 +0200

    Update dawn2.lua

--- luci-app-dawn2/usr/lib/lua/luci/controller/dawn2.lua
+++ luci-app-dawn2/usr/lib/lua/luci/controller/dawn2.lua
@@ -1,5 +1,5 @@
 module("luci.controller.dawn2", package.seeall)
 
 function index()
-    entry({"admin", "network", "dawn2"}, view("dawn2/dawn2"), _("DAWN2"), 91)
+    entry({"admin", "mesh", "dawn2"}, view("dawn2/dawn2"), _("DAWN2"), 91)
 end
