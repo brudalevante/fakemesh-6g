fc612b704581ff6273d469ce70e1bba3f99c91e1
brudalevante <continentalwl@gmail.com>
2025-07-26 23:18:56 +0200

    Añadidos y renombrados paquetes luci-app-dawn2 y luci-app-usteer2

---
+++ luci-app-dawn2/www/luci-static/resources/i18n/dawn2.es.js
@@ -0,0 +1,10 @@
+'use strict';
+(function() {
+    return {
+        "6G Band Metric": "Métrica de banda 6G",
+        "Enable 6G Band": "Activar banda 6G",
+        "Channel Utilization": "Utilización del canal",
+        "Client Utilization": "Utilización de clientes",
+        "RSSI Threshold": "Umbral RSSI"
+    };
+})();
