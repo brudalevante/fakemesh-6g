e87924df2fa9896c269cea4b76ca16717437e3af
brudalevante <continentalwl@gmail.com>
2025-07-28 19:32:12 +0200

    Añadidos y renombrados paquetes luci-app-dawn2

--- luci-app-dawn2/usr/lib/lua/luci/controller/dawn.lua
+++ luci-app-dawn2/usr/lib/lua/luci/controller/dawn.lua
@@ -1,5 +1,5 @@
 module("luci.controller.dawn", package.seeall)
 
 function index()
-    entry({"admin", "mesh", "dawn2"}, view("dawn2/dawn2"), _("DAWN2"), 91)
+    entry({"admin", "mesh", "dawn2"}, view("dawn/dawn"), _("DAWN2"), 91)
 end
