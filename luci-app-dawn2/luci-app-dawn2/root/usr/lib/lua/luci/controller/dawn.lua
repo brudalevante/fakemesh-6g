97d2c9861d9ba37e96766ac2aeaacf6c6647685f
brudalevante <continentalwl@gmail.com>
2025-07-31 19:45:24 +0200

    Delete luci-app-dawn2/luci-app-dawn2 directory

--- luci-app-dawn2/luci-app-dawn2/root/usr/lib/lua/luci/controller/dawn.lua
+++
@@ -1,4 +0,0 @@
-module("luci.controller.dawn", package.seeall)
-function index()
-    entry({"admin", "mesh", "dawn2"}, view("dawn2/dawn"), _("DAWN2"), 91)
-end
--- luci-app-dawn2/luci-app-dawn2/root/www/luci-static/resources/i18n/dawn.es.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    "DAWN": "DAWN",
-    "DAWN Settings": "Configuración de DAWN",
-    "Save": "Guardar",
-    "Cancel": "Cancelar"
-    // Añade aquí más claves y traducciones según tu app
-};
